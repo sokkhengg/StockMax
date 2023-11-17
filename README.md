dea195a91f4135a62489b92913b1ceb8243c88e4
sokkhengg <sokhengteangg@gmail.com>
2023-11-16 21:46:38 -0800

    Add files

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# CPSC-335-Algorithms-Project-2
+# Project 2: Exhaustive vs. Dynamic Programing
+
+## Author & Email
+- Author: Sokheng Teang, Chance Noonan, Cesar Rojas Rojas
+- Email: [sokhengteang@csu.fullerton.edu]
+
+## Abstract
+In this project, we will design, implement and analyze an exhaustive search algorithm dynamic programming algorithm for solving the same problem.  
+
+
+## Problem Statement
+![problem statement](hw2.png)
+
+
+## Setup and Running
+
+### Prerequisites
+
+- Python 3.x
+
+### Instructions
+
+1. Ensure you have Python installed on your machine.
+2. Clone or download this repository.
+3. Navigate to the directory containing `Project1_starter.py`.
+4. Run the Python script using the command:
+```bash
+python3 max_stock.py
---
+++ max_stock.py
@@ -0,0 +1,73 @@
+from itertools import combinations
+
+def total_value(combination):
+    return sum(item[1] for item in combination)
+
+def total_stocks(combination):
+    return sum(item[0] for item in combination)
+
+def exhaustive_search(M, items):
+    best = None
+    for r in range(len(items) + 1):
+        for candidate in combinations(items, r):
+            if total_value(candidate) <= M:
+                if best is None or total_stocks(candidate) > total_stocks(best):
+                    best = candidate
+    return best
+
+def dynamic_programming(M, items):
+    dp = [[0 for _ in range(M + 1)] for _ in range(len(items) + 1)]
+
+    for i in range(1, len(items) + 1):
+        for j in range(1, M + 1):
+            stock_count, stock_value = items[i - 1]
+            if stock_value <= j:
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - stock_value] + stock_count)
+            else:
+                dp[i][j] = dp[i - 1][j]
+    
+    return dp[len(items)][M]
+
+def read_test_cases(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    test_cases = []
+    i = 0
+    while i < len(lines):
+        if lines[i].strip():
+            N = int(lines[i].strip())
+            stocks_and_values = [list(map(int, lines[j].strip().split())) for j in range(i + 1, i + N + 1)]
+            amount = int(lines[i + N + 1].strip())
+            test_cases.append((N, stocks_and_values, amount))
+            i += N + 2
+        else:
+            i += 1
+    return test_cases
+
+def write_results_to_file(file_path, results):
+    with open(file_path, 'w') as file:
+        for result in results:
+            file.write(f"Exhaustive Search - Max Stocks: {result['exhaustive']}, Combination: {result['combination']}\n")
+            file.write(f"Dynamic Programming - Max Stocks: {result['dynamic']}\n\n")
+
+# Main execution
+if __name__ == "__main__":
+    input_file_path = 'input.txt'
+    output_file_path = 'output.txt'
+
+    test_cases = read_test_cases(input_file_path)
+    results = []
+
+    for N, stocks_and_values, amount in test_cases:
+        best_combination = exhaustive_search(amount, stocks_and_values)
+        max_stocks_exhaustive = total_stocks(best_combination) if best_combination else 0
+        max_stocks_dynamic = dynamic_programming(amount, stocks_and_values)
+        
+        results.append({
+            'exhaustive': max_stocks_exhaustive,
+            'dynamic': max_stocks_dynamic,
+            'combination': best_combination
+        })
+
+    write_results_to_file(output_file_path, results)
